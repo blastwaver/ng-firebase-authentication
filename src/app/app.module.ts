b8f0c1f91fe7853a5de10e8a77eaf75fb9b92100
Kyeongho Na <blastwaver.k@gmail.com>
2018-01-14 21:48:50 -0500

    Authentication using firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { CoreModule } from "./core/core.module";
+import { config } from "./config";
 import { AppComponent } from './app.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthService } from './core/auth.service';
+import { AngularFireModule } from 'angularfire2';
+import { Router } from '@angular/router';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    CoreModule,
   ],
-  providers: [],
+  providers: [
+    // Router
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyBaeb9jFqbZcJ2WO6L_YfTOD7gvgodF3VE",
+    authDomain: "ng-auth-18cab.firebaseapp.com",
+    databaseURL: "https://ng-auth-18cab.firebaseio.com",
+    projectId: "ng-auth-18cab",
+    storageBucket: "ng-auth-18cab.appspot.com",
+    messagingSenderId: "853684605083"
+  };
